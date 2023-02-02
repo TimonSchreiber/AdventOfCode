732a9fc7d6303ded07b5d0fb4137c96590b1b5c1
Timon Schreiber <timon.schreiber@live.de>
2023-02-02 22:36:48 +0100

    All CrateStacks together.

---
+++ app/src/main/java/adventofcode/aoc2022/Day05/CrateStacks.java
@@ -0,0 +1,23 @@
+package adventofcode.aoc2022.Day05;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public record CrateStacks(Map<Integer, CrateStack> map) {
+
+    public String getTopCrates() {
+        return this.map.values()
+                .stream()
+                .map(CrateStack::peek)
+                .map(Crate::toString)
+                .collect(Collectors.joining());
+    }
+
+    public CrateStack putIfAbsent(Integer key, CrateStack value ) {
+        return this.map.putIfAbsent(key, value);
+    }
+
+    public CrateStack get(Object key) {
+        return this.map.get(key);
+    }
+}
