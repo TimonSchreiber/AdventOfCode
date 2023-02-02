5eaaa9ad88393cfda34dc9d993ff71f4bd43a59b
Timon Schreiber <timon.schreiber@live.de>
2023-02-02 22:34:42 +0100

    Added class for storing Information about a Crate.

---
+++ app/src/main/java/adventofcode/aoc2022/Day05/Crate.java
@@ -0,0 +1,9 @@
+package adventofcode.aoc2022.Day05;
+
+public record Crate(int codePoint) {
+
+    @Override
+    public String toString() {
+        return Character.toString(this.codePoint);
+    }
+}
