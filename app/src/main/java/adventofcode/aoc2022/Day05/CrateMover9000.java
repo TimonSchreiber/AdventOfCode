adda9dac8b4dbd1b12bb3926f692409e956c90da
Timon Schreiber <timon.schreiber@live.de>
2023-02-02 22:35:43 +0100

    CrateMover implementation.

---
+++ app/src/main/java/adventofcode/aoc2022/Day05/CrateMover9000.java
@@ -0,0 +1,11 @@
+package adventofcode.aoc2022.Day05;
+
+public class CrateMover9000 implements CrateMover {
+
+    @Override
+    public void move(CrateStacks crateStacks, Procedure procedure) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
---
+++ app/src/main/java/adventofcode/aoc2022/Day05/CrateMover9001.java
@@ -0,0 +1,11 @@
+package adventofcode.aoc2022.Day05;
+
+public class CrateMover9001 implements CrateMover {
+
+    @Override
+    public void move(CrateStacks crateStacks, Procedure procedure) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
