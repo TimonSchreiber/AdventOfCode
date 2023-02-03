70d381b2983694d913bf04874d000004567f6123
Timon Schreiber <timon.schreiber@live.de>
2023-02-03 16:28:40 +0100

    Marker record.

---
+++ app/src/main/java/adventofcode/aoc2022/Day06/Marker.java
@@ -0,0 +1,24 @@
+package adventofcode.aoc2022.Day06;
+
+public record Marker(String marker) {
+    
+    /**
+     * Check if every character in the Marker is unique.
+     * 
+     * @return  {@code true} if the number of unique chars in this Marker is
+     *          equal to its length, {@code false} otherwise.
+     */
+    public boolean allUniqueChars() {
+        return this.unique() == this.size();
+    }
+
+    /** the size (length) of this {@code Marker}. */
+    private int size() {
+        return this.marker.length();
+    }
+
+    /** Nu,ber of unique chars in this {@code Marker}. */
+    private long unique() {
+        return this.marker.codePoints().distinct().count();
+    }
+}
