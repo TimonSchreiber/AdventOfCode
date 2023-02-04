d6215756ee2e40977d310fe456994d1ac40f8a6d
Timon Schreiber <timon.schreiber@live.de>
2023-02-04 18:19:53 +0100

    Added FileSystemParser class.

---
+++ app/src/main/java/adventofcode/aoc2022/Day07/FileSystemParser.java
@@ -0,0 +1,75 @@
+package adventofcode.aoc2022.Day07;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FileSystemParser {
+
+    private static FileSystem fileSystem;
+
+    private final static String COMMAND          = "$";
+    private final static String CHANGE_DIRECTORY = "cd";
+    private final static String BACK_UP          = "..";
+    private final static String ROOT             = "/";
+    private final static String LIST             = "ls";
+    private final static String DIRECTORY        = "dir";
+    
+    public static FileSystem parse(List<String> list) {
+        fileSystem = new FileSystem();
+
+        list.stream()
+            .forEachOrdered(FileSystemParser::parse);
+
+        return fileSystem;
+    }
+
+    private static void parse(String string) {
+        // split string along spaces
+        final List<String> line = Arrays.stream(string.split("[ ]")).toList();
+
+        switch (line.get(0)) {
+            case COMMAND   -> parseCommand(line);
+            case DIRECTORY -> parseDirectory(line);
+            default        -> parseFile(line);
+            // default -> the first String is a number
+        }
+    }
+
+    private static void parseCommand(final List<String> line) {
+        // the String behind '$' -> 'cs' or 'ls'
+        switch (line.get(1)) {
+            case CHANGE_DIRECTORY -> parseChangeDirectory(line);
+            case LIST             -> { /* do nothing */ }
+            default               -> invalidCommand(line);
+        }
+    }
+
+    private static void parseDirectory(final List<String> line) {
+        // the String behind 'dir' is the directoy name
+        fileSystem.addDirectory(line.get(1));
+    }
+
+    private static void parseFile(final List<String> line) {
+        // the line starts with a number -> fileSize and fileName
+        long fileSize = Long.parseLong(line.get(0));
+        String fileName = line.get(1);
+    
+        File file = new File(fileSize, fileName);
+        fileSystem.addFile(file);
+    }
+
+    private static void parseChangeDirectory(final List<String> line) {
+        // the String behind 'cd' -> '..', '/', 'x'
+        switch (line.get(2)) {
+            case BACK_UP ->  fileSystem.cdUp();
+            case ROOT    ->  fileSystem.cdRoot();
+            default      ->  fileSystem.cdDown(line.get(2));
+        }
+    }
+
+    private static void invalidCommand(final List<String> line) {
+        throw new IllegalArgumentException("Invalid command: " + line.stream().collect(Collectors.joining(" ")));
+    }
+    
+}
