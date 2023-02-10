d77227487db1b03102eb725a55d77fb8b43c6133
Timon Schreiber <timon.schreiber@live.de>
2023-02-10 11:37:37 +0100

    Added solution for part 2.

--- app/src/main/java/adventofcode/aoc2022/Day13/DistressSignal.java
+++ app/src/main/java/adventofcode/aoc2022/Day13/DistressSignal.java
@@ -1,5 +1,6 @@
 package adventofcode.aoc2022.Day13;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -9,6 +10,9 @@ import adventofcode.util.IO.ReadInput;
  * https://adventofcode.com/2022/day/13
  */
 public class DistressSignal {
+
+    private static final ListValue DIVIDER_1 = ListValue.of(ListValue.of(new IntValue(2)));
+    private static final ListValue DIVIDER_2 = ListValue.of(ListValue.of(new IntValue(6)));
 
     public static void main(String[] args) {
 
@@ -20,14 +24,15 @@ public class DistressSignal {
         final String input = ReadInput.toSingleStringFrom(filePath);
 
         List<PacketPair> packets = PacketPairParser.parse(input);
-
-        packets.forEach(System.out::println);
-
         int part1 = correctOrder(packets);
         System.out.println("-> Part1: " + part1);
 
-        // int part2 = new TrailFinder(heightMap).findTrail();
-        // System.out.println("-> Part2: " + part2);
+        List<ListValue> listValues = ValueParser.parse(input);
+        listValues.add(DIVIDER_1);
+        listValues.add(DIVIDER_2);
+        Collections.sort(listValues);
+        int part2 = dividerPackets(listValues);
+        System.out.println("-> Part2: " + part2);
     }
 
     private static int correctOrder(List<PacketPair> packets) {
@@ -35,6 +40,14 @@ public class DistressSignal {
                 .filter(i -> packets.get(i).checkOrder())
                 .map(i -> i+1)
                 .sum();
+    }
+
+    private static int dividerPackets(List<ListValue> listValues) {
+        return IntStream.range(0, listValues.size())
+                .filter(i -> DIVIDER_1.equals(listValues.get(i)) || DIVIDER_2.equals(listValues.get(i)))
+                .map(i -> i+1)
+                .reduce((a,b) -> a*b)
+                .orElseThrow();
     }
 
 }
